7bc8d7a575f4fb0ddbbaec8aca74b3f9f72fc54d
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2024-08-16 09:25:24 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rghteous: ['Righteous'],
+        saira: ['Saira']
+      }
+      
+    },
+  },
+  
+  plugins: [],
+}
+
